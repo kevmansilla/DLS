db54588a00d4ca4fa35f2ccfcaf77803369fcd87
Leandro200013 <leandro.lopez@mi.unc.edu.ar>
2022-03-27 22:27:08 -0300

    implementación de más funciones

--- Dibujo.hs
+++ Dibujo.hs
@@ -80,22 +80,26 @@ sem f g h i j k l (Apilar n m x y) = j n m (sem f g h i j k l x) (sem f g h i j 
 sem f g h i j k l (Juntar n m x y) = k n m (sem f g h i j k l x) (sem f g h i j k l y)
 sem f g h i j k l (Encimar x y) = l (sem f g h i j k l x) (sem f g h i j k l y)
 
--- type Pred a = a -> Bool
--- -- Dado un predicado sobre básicas, cambiar todas las que satisfacen
--- -- el predicado por la figura básica indicada por el segundo argumento.
--- cambiar :: Pred a -> a -> Dibujo a -> Dibujo a
+type Pred a = a -> Bool
 
--- -- Alguna básica satisface el predicado.
+-- Dado un predicado sobre básicas, cambiar todas las que satisfacen
+-- el predicado por la figura básica indicada por el segundo argumento.
+cambiar :: Pred a -> a -> Dibujo a -> Dibujo a
+cambiar p n x = mapDib (\x -> if (p x) then n else x) x 
+
+-- Alguna básica satisface el predicado.
 -- anyDib :: Pred a -> Dibujo a -> Bool
 
--- -- Todas las básicas satisfacen el predicado.
+-- Todas las básicas satisfacen el predicado.
 -- allDib :: Pred a -> Dibujo a -> Bool
 
--- -- Los dos predicados se cumplen para el elemento recibido.
--- andP :: Pred a -> Pred a -> Pred a
+-- Los dos predicados se cumplen para el elemento recibido.
+andP :: Pred a -> Pred a -> Pred a
+andP p1 p2 x = p1 x && p2 x
 
--- -- Algún predicado se cumple para el elemento recibido.
--- orP :: Pred a -> Pred a -> Pred a
+-- Algún predicado se cumple para el elemento recibido.
+orP :: Pred a -> Pred a -> Pred a
+orP p1 p2 x = p1 x || p2 x
 
 -- -- Describe la figura. Ejemplos:
 -- --
@@ -109,20 +113,60 @@ sem f g h i j k l (Encimar x y) = l (sem f g h i j k l x) (sem f g h i j k l y)
 -- -- La descripción de cada constructor son sus tres primeros
 -- -- símbolos en minúscula, excepto `Rot45` al que se le agrega el `45`.
 
--- desc :: (a -> String) -> Dibujo a -> String
+desc :: (a -> String) -> Dibujo a -> String
+desc f x = sem f descRot descEsp descRot45 descApi descJun descEnc x
+
+descRot :: String -> String
+descRot x = "Rot"++" "++"("++x++")"
+
+descEsp :: String -> String
+descEsp x = "Esp"++" "++"("++x++")"
+
+descRot45 :: String -> String
+descRot45 x = "Rot45"++" "++"("++x++")"
+
+descApi :: Int -> Int -> String -> String -> String
+descApi i j x y= "Api"++" "++"i"++" "++"j"++" "++"("++x++")"++" "++"("++y++")"
+
+descJun :: Int -> Int -> String -> String -> String
+descJun i j x y = "Jun"++" "++"i"++" "++"j"++" "++"("++x++")"++" "++"("++y++")"
+
+descEnc :: String -> String -> String
+descEnc x y = "Enc"++" "++"("++x++")"++" "++"("++y++")"
 
 -- -- Junta todas las figuras básicas de un dibujo.
 -- basicas :: Dibujo a -> [a]
 
 -- -- Hay 4 rotaciones seguidas.
 
--- esRot360 :: Pred (Dibujo a)
+esRot360 :: Pred (Dibujo a)
+esRot360 (Rotar(Rotar(Rotar(Rotar x)))) = True
+esRot360 (Basica x) = False
+esRot360 (Rotar x) = esRot360 x
+esRot360 (Espejar x) = esRot360 x
+esRot360 (Rot45 x) = esRot360 x
+esRot360 (Apilar i j x y) = esRot360 x || esRot360 y
+esRot360 (Juntar i j x y) = esRot360 x || esRot360 y
+esRot360 (Encimar x y) = esRot360 x || esRot360 y
 
 -- -- Hay 2 espejados seguidos.
--- esFlip2 :: Pred (Dibujo a)
+esFlip2 :: Pred (Dibujo a)
+esFlip2 (Espejar(Espejar x)) = True
+esFlip2 (Basica x) = False
+esFlip2 (Rotar x) = esFlip2 x
+esFlip2 (Espejar x) = esFlip2 x
+esFlip2 (Rot45 x) = esFlip2 x
+esFlip2 (Apilar i j x y) = esFlip2 x || esFlip2 y
+esFlip2 (Juntar i j x y) = esFlip2 x || esFlip2 y
+esFlip2 (Encimar x y) = esFlip2 x || esFlip2 y
 
--- data Superfluo = RotacionSuperflua | FlipSuperfluo
+data Superfluo = RotacionSuperflua | FlipSuperfluo
+               deriving (Show)
 
 -- -- Aplica todos los chequeos y acumula todos los errores, y
 -- -- sólo devuelve la figura si no hubo ningún error.
--- check :: Dibujo a -> Either [Superfluo] (Dibujo a)
+check :: Dibujo a -> Either [Superfluo] (Dibujo a)
+check a | esRot360 a && esFlip2 a = Left [RotacionSuperflua,FlipSuperfluo]
+        | esRot360 a = Left [RotacionSuperflua]
+        | esFlip2 a = Left [FlipSuperfluo]
+        | otherwise = Right a
